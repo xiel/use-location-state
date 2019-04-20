ab9dd7c53a04a83de8f0c3b255dacea6ae34747f
Felix Leupold <felix@xiel.de>
2019-04-20 19:17:18 +0200

    add rollup & typescript

--- packages/query-state-core/__tests__/query-state-core.test.ts
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const queryStateCore = require('..');
-
-describe('query-state-core', () => {
-    it('needs tests');
-});
--- packages/query-state-core/lib/query-state-core.ts
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = queryStateCore;
-
-function queryStateCore() {
-    // TODO
-}
