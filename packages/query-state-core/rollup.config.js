bcce816a8fc9b766deb1ac766e89f9c44e2a1025
Felix Leupold <felix@xiel.de>
2019-04-20 20:31:55 +0200

    add tsconfig

--- packages/query-state-core/rollup.config.js
+++ packages/query-state-core/rollup.config.js
@@ -1,13 +1,20 @@
-// adopted from: https://github.com/rollup/rollup-starter-lib/blob/typescript/rollup.config.js
+// adopted from:
+// - https://github.com/rollup/rollup-starter-lib/blob/typescript/rollup.config.js
+// - https://hackernoon.com/building-and-publishing-a-module-with-typescript-and-rollup-js-faa778c85396
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import typescript from 'rollup-plugin-typescript2';
 import pkg from './package.json';
 
+const typescriptPluginOptions = {
+	tsconfigDefaults: { compilerOptions: { declaration: true } },
+	verbosity: 3,
+};
+
 export default [
 	// no browser-friendly UMD build for now?
 	{
-		input: 'src/main.ts',
+		input: 'src/query-state-core.ts',
 		output: {
 			name: 'QueryStateCore',
 			file: pkg.browser,
@@ -16,16 +23,16 @@ export default [
 		plugins: [
 			resolve(),   // so Rollup can find `ms`
 			commonjs(),  // so Rollup can convert `ms` to an ES module
-			typescript() // so Rollup can convert TypeScript to JavaScript
+			typescript(typescriptPluginOptions) // so Rollup can convert TypeScript to JavaScript
 		]
 	},
 
 	// CommonJS (for Node) and ES module (for bundlers) build.
 	{
-		input: 'src/main.ts',
+		input: 'src/query-state-core.ts',
 		external: Object.keys(pkg.dependencies),
 		plugins: [
-			typescript() // so Rollup can convert TypeScript to JavaScript
+			typescript(typescriptPluginOptions) // so Rollup can convert TypeScript to JavaScript
 		],
 		output: [
 			{ file: pkg.main, format: 'cjs' },
