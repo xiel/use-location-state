5dfa0292b8bbfc90a355de02e203bbe38a2021c7
Felix Leupold <felix@xiel.de>
2019-04-21 13:14:19 +0200

    update example for use-location-state

--- examples/use-location-state/01-simple/src/App.tsx
+++ examples/use-location-state/01-simple/src/App.tsx
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import QueryStateTest from './QueryStateTest'
 
 class App extends Component {
   render() {
@@ -8,21 +9,11 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <QueryStateTest />
         </header>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
