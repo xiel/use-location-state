fa12267e9f8fb8d2ef915340a75777dfddd9b2d8
Felix Leupold <felix@xiel.de>
2019-04-22 01:01:19 +0200

    add test for QueryStateTest

--- src/examples/use-location-state/01-simple/src/QueryStateTest.test.tsx
+++ src/examples/use-location-state/01-simple/src/QueryStateTest.test.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
-import { render } from 'react-testing-library'
+import ReactDOM from 'react-dom'
 import QueryStateTest from './QueryStateTest'
 
-it('QueryStateTest renders without crash/loop', () => {
-  expect(render(<QueryStateTest />))
+it('renders QueryStateTest without crashing', () => {
+  const div = document.createElement('div')
+  ReactDOM.render(<QueryStateTest />, div)
+  ReactDOM.unmountComponentAtNode(div)
 })
