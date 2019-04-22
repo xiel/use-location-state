deccdaf8d8e7f949b986c71c3a18808ec9775827
Felix Leupold <felix@xiel.de>
2019-04-22 14:30:37 +0200

    test(useLocationHashQueryStringInterface): improve/fix tests with new api

--- src/packages/use-location-state/test/use-location-state.test.ts
+++ src/packages/use-location-state/test/use-location-state.test.ts
@@ -1,20 +1,5 @@
 import { act, renderHook } from 'react-hooks-testing-library'
-import { useLocationQueryParams, LocationSearch } from '..'
-
-// QueryStringInterface
-const getSetQSI: LocationSearch = {
-  get search() {
-    return location.hash
-  },
-  set search(newSearch) {
-    location.hash = newSearch
-  },
-}
-
-const getSetFuncsQSI: LocationSearch = {
-  getSearch: () => location.hash,
-  setSearch: newSearch => (location.hash = newSearch),
-}
+import { useLocationQueryParams, useLocationHashQueryStringInterface } from '..'
 
 // reset jest mocked hash
 beforeAll(() => {
@@ -27,23 +12,13 @@ afterEach(() => {
 
 describe('useLocationQueryParams hook with hash GetSetQueryStringInterface', () => {
   it('TODO: ...', () => {
-    const { result } = renderHook(props => useLocationQueryParams(props, getSetQSI), {
-      initialProps: { name: 'Sarah' },
-    })
-    expect(result.current.queryState).toEqual({ name: 'Sarah' })
-
-    act(() => result.current.setQueryState({ name: 'Kim' }))
-
-    expect(location.hash).toEqual('#name=Kim')
-    expect(result.current.queryState).toEqual({ name: 'Kim' })
-  })
-})
-
-describe('useLocationQueryParams hook using getSetFuncsQSI', () => {
-  it('TODO: ...', () => {
-    const { result } = renderHook(props => useLocationQueryParams(props, getSetFuncsQSI), {
-      initialProps: { name: 'Sarah' },
-    })
+    const hashQSI = renderHook(() => useLocationHashQueryStringInterface())
+    const { result } = renderHook(
+      props => useLocationQueryParams(props, { queryStringInterface: hashQSI.result.current }),
+      {
+        initialProps: { name: 'Sarah' },
+      }
+    )
     expect(result.current.queryState).toEqual({ name: 'Sarah' })
 
     act(() => result.current.setQueryState({ name: 'Kim' }))
