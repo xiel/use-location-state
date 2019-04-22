e61cc52e8560d69188f13281b14cc3b5074739be
Felix Leupold <felix@xiel.de>
2019-04-23 01:18:16 +0200

    test(demo):

--- src/examples/use-location-state/01-simple/src/QueryStateTest.tsx
+++ src/examples/use-location-state/01-simple/src/QueryStateTest.tsx
@@ -14,25 +14,26 @@ export default function QueryStateTest(props: Props) {
   return (
     <div>
       <h3>Current queryState:</h3>
-      <pre className="pre-query-state">{JSON.stringify(queryState, null, ' ')}</pre>
+      <pre data-testid="pre-query-state">{JSON.stringify(queryState, null, ' ')}</pre>
 
       <h4>name</h4>
       <p>
         <button type="button" onClick={() => setQueryState({ name: 'Felix' })}>
-          set name: "Felix"
+          name: "Felix"
         </button>
         <button type="button" onClick={() => setQueryState({ name: 'Kim' })}>
-          set name: "Kim"
+          name: "Kim"
         </button>
         <button type="button" onClick={() => setQueryState({ name: 'Sarah' })}>
-          set name: "Sarah" (default value)
+          name: "Sarah" (default value)
         </button>
+        <label htmlFor="input-name">name:</label>
         <input
+          id="input-name"
           type="text"
           placeholder={`${queryState.name}`}
           onChange={e => setQueryState({ name: e.target.value })}
         />
-        <button type="button">☑️</button>
       </p>
 
       <h4>Age</h4>
@@ -46,24 +47,25 @@ export default function QueryStateTest(props: Props) {
         <button type="button" onClick={() => setQueryState({ age: 25 })}>
           age: 25
         </button>
+        <label htmlFor="input-age">age:</label>
         <input
+          id="input-age"
           type="text"
           placeholder={`${queryState.age}`}
           onChange={e => setQueryState({ age: parseFloat(e.target.value) })}
         />
-        <button type="button">☑️</button>
       </p>
 
       <h4>name & age</h4>
       <p>
         <button type="button" onClick={() => setQueryState({ name: 'Felix', age: 30 })}>
-          name: 'Felix', age: 30
+          name: "Felix", age: 30
         </button>
         <button type="button" onClick={() => setQueryState({ name: 'Kim', age: 45 })}>
-          name: 'Kim', age: 45
+          name: "Kim", age: 45
         </button>
         <button type="button" onClick={() => setQueryState({ name: 'Sarah', age: 25 })}>
-          name: 'Sarah', age: 25
+          name: "Sarah", age: 25
         </button>
       </p>
     </div>
