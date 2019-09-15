1bc1f2344a6e575972f8be1f55b476d951c97620
Felix Leupold <felix@xiel.de>
2019-09-15 17:26:09 +0200

    make replace default method in hash QSI

--- src/packages/react-router-use-location-state/src/useHistoryQueryState.ts
+++ src/packages/react-router-use-location-state/src/useHistoryQueryState.ts
@@ -4,7 +4,7 @@ import { QueryStringInterface } from 'use-location-state/dist/hooks/types'
 export function useReactRouterQueryStringInterface(history: H.History): QueryStringInterface {
   return {
     getQueryString: () => history.location.search,
-    setQueryString: (newQueryString, { method = 'replace' }) => {
+    setQueryString: (newQueryString, { method = 'replace' }) =>{
       history[method](`${history.location.pathname}?${newQueryString}${history.location.hash}`)
     },
   }
--- src/packages/use-location-state/src/hooks/useLocationHashQueryStringInterface.ts
+++ src/packages/use-location-state/src/hooks/useLocationHashQueryStringInterface.ts
@@ -17,11 +17,11 @@ export function useLocationHashQueryStringInterface({
         if (!enabled) return ''
         return window.location.hash
       },
-      setQueryString: (newQueryString: string, { method }) => {
+      setQueryString: (newQueryString: string, { method = 'replace' }) => {
         if (!enabled) return
         if (window.history && window.history.replaceState) {
           window.history[method === 'replace' ? 'replaceState' : 'pushState'](
-            null,
+            undefined,
             '',
             '#' + newQueryString
           )
