52d39591abecdc9138ebca3a29e49d0fee69dc69
Felix Leupold <felix@xiel.de>
2021-11-12 15:48:43 +0100

    test: fix LocationStateDemo

--- src/examples/use-location-state/src/pages/test/LocationStateTest.test.tsx
+++ src/examples/use-location-state/src/pages/test/LocationStateTest.test.tsx
@@ -3,15 +3,21 @@ import { act, cleanup, fireEvent, render } from '@testing-library/react'
 import LocationStateDemo from '../LocationStateDemo'
 
 afterEach(() => {
+  // Restore all spyOn mocks
+  jest.restoreAllMocks()
+
+  window.history.replaceState(null, '', '/')
   cleanup()
 })
 
 describe('LocationStateDemo', () => {
   test('LocationStateDemo renders without crash/loop', () => {
+    expect(window.history.state).toBe(null)
     expect(render(<LocationStateDemo />))
   })
 
   test('can set name with button', () => {
+    expect(window.history.state).toBe(null)
     const { getByText, getByTestId } = render(<LocationStateDemo />)
 
     // should put new names into the hash (and age default value comes along)
@@ -27,11 +33,14 @@ describe('LocationStateDemo', () => {
   })
 
   test('can set name with text field', () => {
-    const { getByLabelText } = render(<LocationStateDemo />)
+    expect(window.history.state).toBe(null)
+    const { getByLabelText, getByTestId } = render(<LocationStateDemo />)
     fireEvent.change(getByLabelText('name:'), { target: { value: 'Mila' } })
+    expect(getByTestId('pre-query-state')).toMatchSnapshot()
   })
 
   test('can set age with button', () => {
+    expect(window.history.state).toBe(null)
     const { getByText, getByTestId } = render(<LocationStateDemo />)
     act(() => void fireEvent.click(getByText('age: 30')))
     expect(getByTestId('pre-query-state')).toMatchSnapshot()
@@ -42,12 +51,14 @@ describe('LocationStateDemo', () => {
   })
 
   test('can set age with text field', () => {
+    expect(window.history.state).toBe(null)
     const { getByLabelText, getByTestId } = render(<LocationStateDemo />)
     fireEvent.change(getByLabelText('age:'), { target: { value: '33' } })
     expect(getByTestId('pre-query-state')).toMatchSnapshot()
   })
 
   test('can set name & age with button', () => {
+    expect(window.history.state).toBe(null)
     const { getByText, getByTestId } = render(<LocationStateDemo />)
 
     act(() => void fireEvent.click(getByText('name: "Felix", age: 30')))
@@ -62,14 +73,18 @@ describe('LocationStateDemo', () => {
   })
 
   test('can set active with checkbox', () => {
+    expect(window.history.state).toBe(null)
     const { getByLabelText, getByTestId } = render(<LocationStateDemo />)
-    fireEvent.click(getByLabelText('active'))
+    act(() => {
+      fireEvent.click(getByLabelText('active'))
+    })
     expect(getByTestId('pre-query-state')).toMatchSnapshot()
   })
 
-  test.skip('can set active with checkbox - push', () => {
+  test('can set active with checkbox - push', () => {
     const replaceState = jest.spyOn(window.history, 'replaceState')
     const pushState = jest.spyOn(window.history, 'pushState')
+    expect(window.history.state).toBe(null)
     const { getByLabelText, getByTestId } = render(<LocationStateDemo />)
     expect(replaceState).toBeCalledTimes(0)
     expect(pushState).toBeCalledTimes(0)
@@ -79,19 +94,31 @@ describe('LocationStateDemo', () => {
     expect(getByTestId('pre-query-state')).toMatchSnapshot()
   })
 
-  test.skip('can set date with date field', () => {
+  test('can set date with date field', () => {
+    expect(window.history.state).toBe(null)
     const { getByLabelText, getByTestId } = render(<LocationStateDemo />)
-    fireEvent.change(getByLabelText('date:'), {
-      target: { value: '2019-05-01' },
+    act(() => {
+      fireEvent.change(getByLabelText('date:'), {
+        target: { value: '2019-05-01' },
+      })
     })
     expect(getByTestId('pre-query-state')).toMatchSnapshot()
   })
 
-  test.skip('can set null in date field', () => {
+  test('can set null in date field', () => {
+    expect(window.history.state).toBe(null)
     const { getByLabelText, getByTestId } = render(<LocationStateDemo />)
-    fireEvent.change(getByLabelText('date:'), { target: { value: null } })
+    act(() => {
+      fireEvent.change(getByLabelText('date:'), {
+        target: { value: null },
+      })
+    })
     expect(getByTestId('pre-query-state')).toMatchSnapshot()
-    fireEvent.change(getByLabelText('date:'), { target: { value: '' } })
+    act(() => {
+      fireEvent.change(getByLabelText('date:'), {
+        target: { value: '' },
+      })
+    })
     expect(getByTestId('pre-query-state')).toMatchSnapshot()
   })
 })
