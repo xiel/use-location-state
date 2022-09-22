80920e9e46d56ddabd914d1bd3fdbae3c34e4056
Felix Leupold <felix@xiel.de>
2022-09-22 15:32:41 +0200

    fix: SSR warning

--- src/packages/use-location-state/src/hooks/useRefLatest.ts
+++ src/packages/use-location-state/src/hooks/useRefLatest.ts
@@ -1,9 +1,12 @@
-import { useLayoutEffect, useRef } from 'react'
+import { useEffect, useLayoutEffect, useRef } from 'react'
+
+const isSSR = typeof window === 'undefined' || typeof document === 'undefined'
+const useIsoLayoutEffect = isSSR ? useEffect : useLayoutEffect
 
 export function useRefLatest<T>(value: T) {
   const ref = useRef(value)
 
-  useLayoutEffect(() => {
+  useIsoLayoutEffect(() => {
     ref.current = value
   }, [value])
 
