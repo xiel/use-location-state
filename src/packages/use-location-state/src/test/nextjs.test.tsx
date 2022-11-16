c4afb6a7227292b82a895d8500601c63122d9c3d
Felix Leupold <felix@xiel.de>
2022-11-16 12:29:39 +0100

    test: add test for nextjs router

--- src/packages/use-location-state/src/test/nextjs.test.tsx
+++ src/packages/use-location-state/src/test/nextjs.test.tsx
@@ -1,10 +1,16 @@
 import { EMPTY_ARRAY_STRING } from 'query-state-core'
 import { act, renderHook } from '@testing-library/react-hooks'
-import { useQueryState, getServerSideProps, useQueryReducer } from '../next'
+import { getServerSideProps, useQueryReducer, useQueryState } from '../next'
 import * as NextExportsInRoot from '../../next'
-import { unwrapResult } from 'use-location-state-test-helpers/test-helpers'
+import {
+  unwrapABResult,
+  unwrapResult,
+} from 'use-location-state-test-helpers/test-helpers'
 import mockRouter from 'next-router-mock'
 import { useRouter } from 'next/router'
+
+import { render, screen } from '@testing-library/react'
+import { BrowserRouter as wrapper } from 'react-router-dom'
 
 jest.mock('next/router', () => require('next-router-mock'))
 
@@ -83,4 +89,96 @@ test('Exports for next in root folder', () => {
     'useQueryReducer',
     'useQueryState',
   ])
+})
+
+describe('Resetting to original value in batch with a following no-op', () => {
+  test('using clicks', () => {
+    const onRender = jest.fn()
+
+    function Comp() {
+      const [name, setName] = useQueryState('name', '')
+      const [age, setAge] = useQueryState('age', 18)
+      const state = { age, name }
+      onRender(state)
+      return (
+        <>
+          <button
+            onClick={() => {
+              setName('Ron')
+              setAge(18)
+            }}
+          >
+            set
+          </button>
+          <button
+            onClick={() => {
+              setName('')
+              setAge(18)
+            }}
+          >
+            reset
+          </button>
+        </>
+      )
+    }
+
+    render(<Comp />, { wrapper })
+
+    expect(window.location.search).toEqual('')
+    screen.getByRole('button', { name: 'set' }).click()
+
+    expect(onRender).toHaveBeenCalledTimes(2)
+    expect(onRender).toHaveBeenLastCalledWith({
+      age: 18,
+      name: 'Ron',
+    })
+    expect(window.location.search).toEqual('?name=Ron')
+
+    screen.getByRole('button', { name: 'reset' }).click()
+    expect(window.location.search).toEqual('')
+  })
+
+  test(`using setter from hook directly`, () => {
+    const { result, unmount } = renderHook(
+      () => ({
+        a: useQueryState('age', 18),
+        b: useQueryState('names', [] as string[]),
+      }),
+      { wrapper }
+    )
+
+    const { a: age, b: names } = unwrapABResult(result)
+
+    // default
+    expect(result.error).toBe(undefined)
+    expect(window.location.search).toEqual('')
+    expect(age.value).toEqual(18)
+    expect(names.value).toEqual([])
+
+    // set next values:
+    act(() => {
+      age.setValue(31)
+      names.setValue([])
+    })
+
+    // check new value
+    expect(result.error).toBe(undefined)
+    expect(window.location.search).toEqual('?age=31')
+    expect(age.value).toEqual(31)
+    expect(names.value).toEqual([])
+
+    // reset to original values
+    act(() => {
+      age.setValue(18)
+      names.setValue([])
+    })
+
+    // check successful reset
+    expect(result.error).toBe(undefined)
+    expect(window.location.search).toEqual('')
+    expect(age.value).toEqual(18)
+    expect(names.value).toEqual([])
+
+    unmount()
+  })
 })
